3df9bd418ef6e80075e50c4b5634cb5a0e551c8d
zhr <zhr15030727447@gmail.com>
2025-01-24 15:50:12 +0800

    commit

---
+++ c/String_function/my_strlen.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+//模拟器strlen函数
+
+//计数器方法
+/*
+size_t my_strlen(const char* str)
+{
+    size_t count=0;
+    assert(str);
+    while(*str !='\0')
+    {
+        count++;
+        str++;
+    }
+    return count;
+}
+int main()
+{
+    char arr[]={"abcdef"};
+    size_t n = my_strlen(arr);
+    printf("%zu\n",n);
+    return 0;
+}
+*/
+
+//指针-指针
+/*
+size_t my_strlen(const char* str)
+{
+    const char *start=str;
+    while(*str)
+    {
+        str++;
+    }
+    return str-start;
+}
+int main()
+{
+    char arr[]="abcdef";
+    size_t n=my_strlen(arr);
+    printf("%zu\n",n);
+    return 0;
+}
+*/
+
+//递归方法
+/*
+size_t my_strlen(const char* str)
+{
+    if(*str=='\0')
+    {
+        return 0;
+    }
+    return 1+my_strlen(str+1);
+}
+int main()
+{
+    char arr[]="abcdef";
+    size_t n=my_strlen(arr);
+    printf("%zu\n",n);
+    return 0;
+}
+*/
---
+++ c/my_strcpy_strcat.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+//模拟实现strcpy函数
+/*
+char* my_strcpy(char* dest,const char* src)
+{
+    assert(dest && src);
+    char* ret = dest;
+    while(*dest++ = *src++)
+        ;
+    return ret;
+
+}
+int main()
+{
+    char arr1[]="abcdef";
+    char arr2[]={0};
+    my_strcpy(arr2,arr1);
+    printf("%s\n",arr2);
+    return 0;
+}
+*/
+//模拟实现strcat函数
+
+char* my_strcat(char* dest,const char* src)
+{
+    char* ret=dest;
+    assert(dest && src);
+    while(*dest != '\0')
+    {
+        dest++;
+    }
+
+    while(*dest++ = *src++)
+        ;
+    return ret;
+}
+int main()
+{
+    char arr[]="hello ";
+    my_strcat(arr,"world");
+    printf("%s\n",arr);
+    return 0;
+}
