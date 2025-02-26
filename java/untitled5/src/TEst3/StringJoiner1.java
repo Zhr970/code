d846d35ad650bd8b060e9bdb88e90b5ebf9abe62
zhr <zhr15030727447@gmail.com>
2025-02-26 17:45:38 +0800

    commit

--- java/untitled5/src/TEst3/StringJoiner1.java
+++ java/untitled5/src/TEst3/StringJoiner1.java
@@ -1,10 +1,24 @@
 package TEst3;
 
-public class StringJoiner {
+import java.util.StringJoiner;
+
+public class StringJoiner1 {
     public static void main(String[] args) {
         //
         StringJoiner sj = new StringJoiner("---");
         sj.add("aaa").add("bbb");
         System.out.println(sj);
+
+        //
+        StringJoiner sj2 = new StringJoiner("---","[","]");
+        sj2.add("qqq").add("www").add("eee");
+        System.out.println(sj2);
+
+        int len = sj2.length();
+        System.out.println(len);//17
+
+        String str = sj2.toString();
+        System.out.println(str);
+
     }
 }
--- java/untitled6/src/Test1/String1.java
+++ java/untitled6/src/Test1/String1.java
@@ -1,4 +1,32 @@
 package Test1;
 
 public class String1 {
+    public static void main(String[] args) {
+
+        String s1 = "abc";
+        String s2 = "ab";
+        String s3 = s2+"c";
+        System.out.println(s1==s3);//false
+
+        String s4 = "a"+"b"+"c";
+        System.out.println(s1==s4);//ture
+
+        StringBuffer sb = new StringBuffer();
+        System.out.println();
+        System.out.println(sb.capacity());
+        System.out.println(sb.length());
+
+        System.out.println();
+        sb.append("abc");
+        System.out.println(sb.capacity());
+        System.out.println(sb.length());
+
+        System.out.println();
+        sb.append("---abcdefghijklmn");
+        System.out.println(sb.capacity());
+        System.out.println(sb.length());
+
+
+
+    }
 }
